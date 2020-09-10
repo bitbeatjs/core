d0a39570d1e6228645f404e9a6d44526717c8812
Oliver Freudrich <info@oliverfreudrich.com>
2020-09-10 10:47:52 +0200

    - removed documentation zipping => TODO: fix not zip node_modules

--- bin/zip.sh
+++ bin/zip.sh
@@ -9,15 +9,6 @@ include_documentation=false
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
 while true; do
-    read -p "Do you want to only zip the documentation (${yesword} / ${noword})? " yn
-    case $yn in
-        ${yesptrn##^} ) documentation_only=true; break;;
-        ${noptrn##^} ) documentation_only=false; break;;
-        * ) echo "Answer ${yesword} / ${noword}.";;
-    esac
-done
-
-while true; do
     read -p "Do you want to include the node_modules (${yesword} / ${noword})? " yn
     case $yn in
         ${yesptrn##^} ) node_modules=true; break;;
@@ -26,35 +17,14 @@ while true; do
     esac
 done
 
-if [ "$documentation_only" = false ]; then
-  while true; do
-    read -p "Include the documentation (${yesword} / ${noword})? " yn
-    case $yn in
-        ${yesptrn##^} ) include_documentation=true; break;;
-        ${noptrn##^} ) include_documentation=false; break;;
-        * ) echo "Answer ${yesword} / ${noword}.";;
-    esac
-  done
+files=("*")
+files_string="$(join_by " " "${files[@]}")"
 
-  files=("package*.json" "LICENSE.md" "README.md" "index.js" "index.d.ts" "__core__/*.js" "__core__/*.d.ts" "__core__/**/*.js" "__core__/**/*.d.ts" "bin/*.js" "bin/*.d.ts" "docker/core" ".dockerignore" "locales" "docker-compose.yml")
-
-  if [ "$include_documentation" = true ]; then
-    files+=("docs" "docker-compose.docs.yml" "docker/docs")
-  fi;
-
-  if [ "$node_modules" = true ]; then
-    files+=("node_modules")
-  fi;
-
+if [ "$node_modules" = false ]; then
+  files_string="${files_string[@]//node_modules/""}"
+  # tar -czvf bitbeat_$(date +'%F_%H-%M-%S').tar.gz $files_string --exclude=node_modules
+  echo $files_string --exclude=node_modules
+else
   files_string="$(join_by " " "${files[@]}")"
   tar -czvf bitbeat_$(date +'%F_%H-%M-%S').tar.gz $files_string
-else
-  files=("package*.json" "docs" "docker-compose.docs.yml" ".dockerignore" "docker/docs")
-
-  if [ "$node_modules" = true ]; then
-    files+=("node_modules")
-  fi;
-
-  files_string="$(join_by " " "${files[@]}")"
-  tar -czvf bitbeat_docs_$(date +'%F_%H-%M-%S').tar.gz $files_string
-fi
+fi;
